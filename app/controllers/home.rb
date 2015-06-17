a9b305a66e1e40463dbc660eb4a396dbc63af7b6
Brian Kennedy <briankennedy1@gmail.com>
2015-06-16 23:29:11 -0700

    AJAXXXXXX

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -10,6 +10,22 @@ end
 
 post '/contacts/new' do
   # create new contact
+
+  @contact = Contact.new(params)
+  #   first_name: params[:first_name],
+  #   last_name: params[:last_name],
+  #   street: params[:street],
+  #   city: params[:city],
+  #   state: params[:state],
+  #   zip: params[:zip],
+  #   email: params[:email],
+  #   phone: params[:phone]
+  # )
+  if @contact.save
+    {sendback: @contact}.to_json
+  else
+    {sendback: @contact.errors}.to_json
+  end
 end
 
 get '/contacts/edit/:id' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  addContactFormListener();
+  addNewContactListener();
 });
+
+var addContactFormListener = function(){
+  $('#add-new').on('click', '#add-new-btn', function(e){
+    e.preventDefault();
+    $('#add-new-btn').toggle();
+    $('.new-contact-form').toggle();
+  })
+};
+var addNewContactListener = function(){
+  $('#add-new').on('click', '#add-contact-btn', function(e){
+    e.preventDefault();
+
+    var action = $('.new-contact-form').attr('action');
+
+    var method = $('.new-contact-form').attr('method');
+
+    var data   = $('.new-contact-form').serialize();
+
+  var request = $.ajax({
+    url: action,
+    type: method,
+    data: data,
+    dataType: 'JSON'
+  });
+
+  request.done(function(response){
+    $("tbody").append("<tr>"+ response +"</tr>")
+    console.log("Success")
+  })
+
+  request.fail(function(response){
+    console.log(response)
+    console.log("Fail")
+  })
+
+  })
+};
